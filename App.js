deb07d8afbc7d25e91f7befda66e67399f697a92
Anh Phan <anhphan42891@gmail.com>
2024-09-25 09:32:28 +0700

    add signIn and SingUp

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 import  MainScreen from './screens/MainScreen.js';
 import RegisterScreen from './screens/Register.js';
 import testScreen from './screens/test.js';
+import SignInScreen from './screens/SignIn.jsx';
+import SignUpScreen from './screens/SignUp.jsx';
 const App = () => {
   return <MainScreen/>;
 };
--- Data/images.js
+++ Data/images.js
@@ -6,6 +6,7 @@ export const images = [
     { id: 5, src: require('../assets/Anh5.jpg') ,name: 'Can Tho'},
     { id: 6, src: require('../assets/Anh6.jpg') ,name: 'Ha Noi'},
     { id: 7, src: require('../assets/Anh7.jpg') ,name: 'Da Nang'},
+<<<<<<< Updated upstream
     { id: 8, src: require('../assets/Anh8.jpg'),name: 'TP HCM'},
     { id: 9, src: require('../assets/Anh9.jpg') ,name: 'Can Tho'},
     { id: 10, src: require('../assets/Anh10.jpg') ,name: 'Ha Noi'},
@@ -16,4 +17,16 @@ export const images = [
     { id: 15, src: require('../assets/Anh15.jpg') ,name: 'Da Nang'},
     { id: 16, src: require('../assets/Anh16.jpg') ,name: 'Ha Noi'},
     { id: 17, src: require('../assets/Anh17.jpg') ,name: 'Da Nang'},
+=======
+    { id: 8, src: require('../assets/Anh8.jpg') ,name: 'Nha Trang'},
+    { id: 9, src: require('../assets/Anh9.jpg') ,name: 'Ca Mau'},
+    { id: 10, src: require('../assets/Anh10.jpg') ,name: 'Soc Trang'},
+    { id: 11, src: require('../assets/Anh11.jpg') ,name: 'Bac Lieu'},
+    { id: 12, src: require('../assets/Anh12.jpg') ,name: 'Kien Giang'},
+    { id: 13, src: require('../assets/Anh13.jpg') ,name: 'Phu Quoc'},
+    { id: 14, src: require('../assets/Anh14.jpg') ,name: 'Tra Vinh'},
+    { id: 15, src: require('../assets/Anh15.jpg') ,name: 'Nga Nam'},
+    { id: 16, src: require('../assets/Anh16.jpg') ,name: 'Vinh Long'},
+
+>>>>>>> Stashed changes
   ];
