ad114c1196be488bfa35578a8a1388eadda8d98a
Anh Phan <anhphan42891@gmail.com>
2024-10-04 10:31:03 +0700

    add Thien's Screen

---
+++ Data/imgData.js
@@ -0,0 +1,24 @@
+const imgData = [
+    {
+        id: '0',
+        src: require('./assets/hinh1.png')
+    }
+    ,
+    {
+        id: '2',
+        src: require('./assets/hinh2.png')
+    },
+    {
+        id: '3',
+        src: require('./assets/hinh3.png')
+    },
+    {
+        id: '4',
+        src: require('./assets/hinh4.png')
+    },
+    {
+        id: '5',
+        src: require('./assets/hinh5.png')
+    }
+];
+export default imgData;
